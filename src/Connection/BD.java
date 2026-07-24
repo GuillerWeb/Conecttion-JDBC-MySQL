d64c36dcdbce925c1c03ee0a03c7c147f966829f
GuillerWeb <guilhermesilvapinto471@gmail.com>
2026-07-24 17:03:45 -0400

    Classe de conexão finalizada

---
+++ src/Connection/BD.java
@@ -0,0 +1,40 @@
+package Connection;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class BD{
+
+    // Método para carregar meu db.properties contendo dados sensíveis do Banco de Dados
+    private static Properties loadProperties() throws IOException {
+        try(FileInputStream fileInputstream = new FileInputStream("db.properties")) {
+            Properties properties = new Properties();
+            properties.load(fileInputstream);
+            return properties;
+        } catch (IOException e) {
+            throw new IOException("Erro ao carregar o arquivo db.properties");
+        }
+    }
+
+
+    public static Connection connection = null;
+    //Classe onde a conexão com o Banco vai ser efetuada por meio do arquivo properties e da url
+    public static Connection getConnection() throws IOException, SQLException {
+
+        try{
+            if (connection == null) {
+                Properties properties = loadProperties();
+                String url = properties.getProperty("db.url");
+                connection = DriverManager.getConnection("url", properties);
+
+            }
+        }catch(SQLException sqlException){
+            throw new SQLException("Erro ao conectar ao banco de dados");
+        }
+        return connection;
+    }
+}
